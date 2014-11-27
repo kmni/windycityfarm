bf2dfc7ce8f69dc958000742152a2ed603142fcc
Daniel Bielczyk <daniel@koderskapohotovost.cz>
2014-11-27 14:46:15 +0100

    layout and homepage

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	
+//
+// owl carousel
+//
+(function() {
+	$(".carousel").owlCarousel({
+		navigation : false,
+		singleItem : true,
+		transitionStyle : "fade",
+		autoPlay: 5000
+	});
+}());
+
+});
