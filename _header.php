c26ec44818238143038ee734c304115c47e8f48b
Daniel Bielczyk <daniel@koderskapohotovost.cz>
2014-11-28 15:24:25 +0100

    added meta viewport

--- _header.php
+++ _header.php
@@ -7,6 +7,7 @@
 		<meta name="keywords" content="" />
 		<meta name="author" content="" />
 		<meta name="robots" content="index,follow" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 		<link href="./styles/style.css" rel="stylesheet" media="screen" />
 		<link href="./styles/print.css" rel="stylesheet" media="print" />
